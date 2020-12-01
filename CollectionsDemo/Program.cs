93533cf9b729d8b137cb2fcd01ae53138a15e81b
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-12-01 16:37:38 +0530

    STARTED:collections Demo

---
+++ CollectionsDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Collections collections = new Collections();
+            Console.WriteLine("ListDemo");
+            collections.ListDemo();
+            Console.ReadKey();
+        }
+    }
+}
